b0a700ca3494d98d390a7f94d129bc951d7f16bc
Nico Martinucci <domenico.martinucci@gmail.com>
2023-02-03 12:17:25 -0800

    update file saving

--- files.py
+++ files.py
@@ -7,12 +7,9 @@ s3 = boto3.resource('s3')
 
 def post_new_file(file):
     """ Upload a new file; returns URL to file. """
-    bucket_name = os.environ['AWS_BUCKET']
+    bucket_name = "bybo-rithm"
     file_name = f"{str(uuid.uuid4())}.jpg"
 
     resp = s3.Bucket(bucket_name).put_object(Key=file_name, Body=file)
 
-    if bucket_name == "bybo-rithm":
-        return f"https://{bucket_name}.s3.us-west-1.amazonaws.com/{file_name}"
-    else:
-        return f"https://s3.us-west-1.amazonaws.com/{bucket_name}/{file_name}"
+    return f"https://{bucket_name}.s3.us-west-1.amazonaws.com/{file_name}"
